ba7fd997d74e2d7f516be11033b0a0188640cfc2
asabdulkareem <as.abdulkareem@gmail.com>
2020-11-11 21:18:25 +0530

    Modified   package.json Modified   src/App.js Modified   src/Person/Person.js Styled And Dynamic Styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
+// import Radium, { StyleRoot } from 'radium';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state= {
@@ -39,18 +54,18 @@ class App extends Component {
     this.setState({persons:persons});
   }
   render(){  
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1x solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // }
     let persons = null;
     if(this.state.showPersons){
       persons = (
@@ -66,11 +81,11 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'lightred',
-        color: 'black'
-      }
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'lightred',
+      //   color: 'black'
+      // }
     }
     let classes = [];
     if (this.state.persons.length <= 2)
@@ -82,18 +97,18 @@ class App extends Component {
       classes.push('bold');
     }
     return ( 
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}> This is really working </p>
-          <button 
-            style={style}
-            onClick = {this.togglePersonsHandler}>Toggle Person</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      // <StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}> This is really working </p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
+        {persons}
+      </div>
+      // </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,34 @@
-import Radium from 'radium';
+// import Radium from 'radium';
+import styled from 'styled-components';
 import React from 'react';
-import './Person.css';
+// import './Person.css';
 
-const Person = (props) => {
-    const style = {
-        '@media (min-width: 500 px)': {
-            width: '450px'
-        }
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    @media (min-width:500px){
+        width: 450px;
     }
-    return (
-        <div className="Person" style={style}>
+`;
+const Person = (props) => {
+    // const style = {
+    //     '@media (min-width: 500 px)': {
+    //         width: '450px'
+    //     }
+    // }
+    return (        
+        <StyledDiv>
             <h1 onClick={props.click}>Name: {props.name} </h1>
             <p>{props.children}</p>
             <p>Age: {props.age}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyledDiv>
     )
 }
 
-export default Radium(Person);
+export default Person;
